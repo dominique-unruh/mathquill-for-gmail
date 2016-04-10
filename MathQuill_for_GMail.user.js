103c00397fe5d01f3ab45b63c02424c5832fde7b
Dominique Unruh <unruh@ut.ee>
2016-04-11 00:50:37 +0300

    Added support for data-url-images.
    Unfortunately, they don't work right in GMail.

--- MathQuill_for_GMail.user.js
+++ MathQuill_for_GMail.user.js
@@ -5,6 +5,7 @@
 // @version     1
 // @require     https://code.jquery.com/jquery-2.2.2.min.js
 // @require     https://kodu.ut.ee/~unruh/mathquill-0.10.1/mathquill.min.js
+// @grant       GM_xmlhttpRequest
 // ==/UserScript==
 
 var MQ = MathQuill.getInterface(2);
@@ -41,6 +42,16 @@ function update_pic(img,ltx) {
     img.attr("src",url);
     img.attr("alt",ltx);
     img.attr("title",ltx);
+
+    // TODO: finish
+    get_data_url(url,function (data) { 
+	try {
+	    console.log("got data url",data);
+	    img.attr("src",data);
+	} catch (e) {
+	    console.error(e);
+	}
+    });
 }
 
 function element_before_cursor() {
@@ -189,6 +200,116 @@ function install_css() {
 	console.error(e);
     }
 };
+
+
+// From here: http://stackoverflow.com/a/8781262/2646248
+function customBase64Encode (inputStr) {
+    var
+        bbLen               = 3,
+        enCharLen           = 4,
+        inpLen              = inputStr.length,
+        inx                 = 0,
+        jnx,
+        keyStr              = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+                            + "0123456789+/=",
+        output              = "",
+        paddingBytes        = 0;
+    var
+        bytebuffer          = new Array (bbLen),
+        encodedCharIndexes  = new Array (enCharLen);
+
+    while (inx < inpLen) {
+        for (jnx = 0;  jnx < bbLen;  ++jnx) {
+            /*--- Throw away high-order byte, as documented at:
+              https://developer.mozilla.org/En/Using_XMLHttpRequest#Handling_binary_data
+            */
+            if (inx < inpLen)
+                bytebuffer[jnx] = inputStr.charCodeAt (inx++) & 0xff;
+            else
+                bytebuffer[jnx] = 0;
+        }
+
+        /*--- Get each encoded character, 6 bits at a time.
+            index 0: first  6 bits
+            index 1: second 6 bits
+                        (2 least significant bits from inputStr byte 1
+                         + 4 most significant bits from byte 2)
+            index 2: third  6 bits
+                        (4 least significant bits from inputStr byte 2
+                         + 2 most significant bits from byte 3)
+            index 3: forth  6 bits (6 least significant bits from inputStr byte 3)
+        */
+        encodedCharIndexes[0] = bytebuffer[0] >> 2;
+        encodedCharIndexes[1] = ( (bytebuffer[0] & 0x3) << 4)   |  (bytebuffer[1] >> 4);
+        encodedCharIndexes[2] = ( (bytebuffer[1] & 0x0f) << 2)  |  (bytebuffer[2] >> 6);
+        encodedCharIndexes[3] = bytebuffer[2] & 0x3f;
+
+        //--- Determine whether padding happened, and adjust accordingly.
+        paddingBytes          = inx - (inpLen - 1);
+        switch (paddingBytes) {
+            case 1:
+                // Set last character to padding char
+                encodedCharIndexes[3] = 64;
+                break;
+            case 2:
+                // Set last 2 characters to padding char
+                encodedCharIndexes[3] = 64;
+                encodedCharIndexes[2] = 64;
+                break;
+            default:
+                break; // No padding - proceed
+        }
+
+        /*--- Now grab each appropriate character out of our keystring,
+            based on our index array and append it to the output string.
+        */
+        for (jnx = 0;  jnx < enCharLen;  ++jnx)
+            output += keyStr.charAt ( encodedCharIndexes[jnx] );
+    }
+    return output;
+}
+
+function get_data_url(url,callback) {
+    try {
+	/*
+	var req = new XMLHttpRequest();
+	req.overrideMimeType("text/plain; charset=x-user-defined");
+	req.addEventListener("load",function (evt) { 
+	    console.log("done",evt,req,this);
+	    console.log("base64",btoa(this.responseText));
+	});
+	req.open("GET",url);
+	req.send();
+	*/
+	console.log("GM_xmlhttpRequest");
+	//url = "https://latex.codecogs.com/png.latex?\\dpi{300}\\inline%09\\frac{abcef}{ccc}";
+	GM_xmlhttpRequest({
+	    url: url,
+	    method: "GET",
+	    binary: true,
+	    overrideMimeType: "text/plain; charset=x-user-defined",
+	    onload: function (resp) {
+		try {
+		    console.log("done",resp,this,resp.finalUrl);
+		    dataurl = "data:image/png;base64,"+customBase64Encode(resp.responseText);
+		    callback(dataurl);
+		} catch (e) {
+		    console.error(e);
+		}
+	    },
+	    onerror: function (resp) {
+		try {
+		    console.log("Could not load",url,resp);
+		} catch (e) {
+		    console.error(e);
+		}
+	    },
+	});
+	console.log("GM_xmlhttpRequest invoked");
+    } catch (e) {
+	console.error(e);
+    }
+}
 
 install_css();
 install_image_click_handler();
