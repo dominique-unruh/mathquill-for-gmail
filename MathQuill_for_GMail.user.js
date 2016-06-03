28b6885b6929e3911c2bb63faac514a730c03873
Dominique Unruh <unruh@ut.ee>
2016-06-04 01:29:08 +0300

    fixed indentation (indent level = 2, no tabs)

--- MathQuill_for_GMail.user.js
+++ MathQuill_for_GMail.user.js
@@ -25,137 +25,137 @@ var current_math = null;
   img -- a jQuery object containing one img
 */
 function update_pic(img,ltx) {
-    var url = "https://latex.codecogs.com/png.latex?\\dpi{300}\\inline%09" + encodeURIComponent(ltx);
-    if (ltx=="") {
-        ltx = "<empty math>";
-        url = "https://latex.codecogs.com/png.latex?\\dpi{300}\\inline%09?";
-    }
-	img.one("load",function () {
-	    try {
-		var width = (img[0].naturalWidth*0.36)+"em";
-		//console.log("img loaded",width,img[0].naturalWidth,img[0].naturalHeight);
-		img.attr("width",width);
-		img.attr("height","auto");
+  var url = "https://latex.codecogs.com/png.latex?\\dpi{300}\\inline%09" + encodeURIComponent(ltx);
+  if (ltx=="") {
+    ltx = "<empty math>";
+    url = "https://latex.codecogs.com/png.latex?\\dpi{300}\\inline%09?";
+  }
+  img.one("load",function () {
+    try {
+      var width = (img[0].naturalWidth*0.36)+"em";
+      //console.log("img loaded",width,img[0].naturalWidth,img[0].naturalHeight);
+      img.attr("width",width);
+      img.attr("height","auto");
 
-		if (img[0].naturalHeight >= 45) {
-		    img.attr("style","vertical-align: middle");
-		} else
-		    img.removeAttr("style");
-	    } catch (e) {
-		console.error(e);
-	    }
-	});
-    img.attr("style","border: dashed 2px green; filter: blur(.5px)");
-    img.attr("src",url);
-    img.attr("alt",ltx);
-    img.attr("title",ltx);
+      if (img[0].naturalHeight >= 45) {
+        img.attr("style","vertical-align: middle");
+      } else
+        img.removeAttr("style");
+    } catch (e) {
+      console.error(e);
+    }
+  });
+  img.attr("style","border: dashed 2px green; filter: blur(.5px)");
+  img.attr("src",url);
+  img.attr("alt",ltx);
+  img.attr("title",ltx);
 }
 
 function element_before_cursor() {
-    var sel = document.getSelection();
-    if (!sel.isCollapsed) return null;
-    var range = sel.getRangeAt(0);
-    var idx = range.startOffset - 1;
-    if (idx < 0) return null;
-    var node = range.startContainer.childNodes[idx];
-    if (! (node instanceof Element)) return null;
-    return node;
+  var sel = document.getSelection();
+  if (!sel.isCollapsed) return null;
+  var range = sel.getRangeAt(0);
+  var idx = range.startOffset - 1;
+  if (idx < 0) return null;
+  var node = range.startContainer.childNodes[idx];
+  if (! (node instanceof Element)) return null;
+  return node;
 };
 
 function mq_close(math) {
-    try {
-	//console.log("Closing MQ");
-        current_math = null;
-	var img = $("#"+math.el().id+"-image");
-	var ltx = math.latex();
-	update_pic(img,ltx);
-	$(math.el()).remove();
+  try {
+    //console.log("Closing MQ");
+    current_math = null;
+    var img = $("#"+math.el().id+"-image");
+    var ltx = math.latex();
+    update_pic(img,ltx);
+    $(math.el()).remove();
 
-	// Put cursor after img
-	if (previousActiveElement!=null) previousActiveElement.focus();
-	var sel = document.getSelection();
-        sel.removeAllRanges();
-        var range = document.createRange();
-        range.setStartAfter(img[0]);
-        range.setEndAfter(img[0]);
-        sel.addRange(range);
+    // Put cursor after img
+    if (previousActiveElement!=null) previousActiveElement.focus();
+    var sel = document.getSelection();
+    sel.removeAllRanges();
+    var range = document.createRange();
+    range.setStartAfter(img[0]);
+    range.setEndAfter(img[0]);
+    sel.addRange(range);
 
-	//console.log("Closed MQ");
-    } catch (e) {
-	console.error(e);
-	return;
-    }
+    //console.log("Closed MQ");
+  } catch (e) {
+    console.error(e);
+    return;
+  }
 }
 
 function edit_math(img) {
-    var id = img.attr("mathquill-id");
-    var latex = img.attr("alt");
-    if (latex=="<empty math>") latex = "";
+  var id = img.attr("mathquill-id");
+  var latex = img.attr("alt");
+  if (latex=="<empty math>") latex = "";
 
-    previousActiveElement = document.activeElement;
+  previousActiveElement = document.activeElement;
 
-    if (id!==null) {
-        var m = $("#"+id);
-        if (m.length>0) {
-            current_math = MQ(m[0]).focus();
-            return;
-        }
+  if (id!==null) {
+    var m = $("#"+id);
+    if (m.length>0) {
+      current_math = MQ(m[0]).focus();
+      return;
     }
+  }
 
-	id = ("mq-"+Math.random()).replace(".","");
-	img.attr("mathquill-id",id);
-	img.attr("id",id+"-image");
+  id = ("mq-"+Math.random()).replace(".","");
+  img.attr("mathquill-id",id);
+  img.attr("id",id+"-image");
 
-    var mathSpan = $("<span>").attr("id",id);
-    img.after(mathSpan);
-    img.attr("style","filter: blur(.5px)");
+  var mathSpan = $("<span>").attr("id",id);
+  img.after(mathSpan);
+  img.attr("style","filter: blur(.5px)");
 
-    var math = MQ.MathField(mathSpan[0], {
-	handlers: { enter: mq_close },
-	});
-    math.latex(latex);
-    math.focus();
-    current_math = math;
+  var math = MQ.MathField(mathSpan[0], {
+    handlers: { enter: mq_close },
+  });
+  math.latex(latex);
+  math.focus();
+  current_math = math;
 }
 
 // img - Element
 function is_mq_img(img) {
-    if (img == null) return false;
-    if (img.tagName != "IMG") return false;
-    if (img.src == null) return false;
-    if (!img.src.includes("https://latex.codecogs.com/")) return false;
-    return true;
+  if (img == null) return false;
+  if (img.tagName != "IMG") return false;
+  if (img.src == null) return false;
+  if (!img.src.includes("https://latex.codecogs.com/")) return false;
+  return true;
 }
 
 function image_handler(event) {
-    //console.log("click-listener:",event.target.tagName,event,document.location);
-    
-    if (event.button != 0) return;
-    if (!is_mq_img(event.target)) return;
-    
-    img = $(event.target);
-    
-    //console.log("Click on",event.target);
-    event.stopPropagation();
+  //console.log("click-listener:",event.target.tagName,event,document.location);
 
-    edit_math(img);
-    
-    //console.log("Click handled");
+  if (event.button != 0) return;
+  if (!is_mq_img(event.target)) return;
+
+  img = $(event.target);
+
+  //console.log("Click on",event.target);
+  event.stopPropagation();
+
+  edit_math(img);
+
+  //console.log("Click handled");
 };
 
 function install_image_click_handler() {
-    window.addEventListener("click",image_handler,true);
+  window.addEventListener("click",image_handler,true);
 };
 
 function create_math() {
-    var img = $("<img>");
-    img.attr("src","https://latex.codecogs.com/png.latex?\\dpi{300}\\inline%09?");
-    img.attr("alt","<empty math>");
-    img.attr("title","<empty math>");
-    img.attr("width","7em");
-    img.attr("height","auto");
-    //update_pic($(img),"");
-    return img[0];
+  var img = $("<img>");
+  img.attr("src","https://latex.codecogs.com/png.latex?\\dpi{300}\\inline%09?");
+  img.attr("alt","<empty math>");
+  img.attr("title","<empty math>");
+  img.attr("width","7em");
+  img.attr("height","auto");
+  //update_pic($(img),"");
+  return img[0];
 }
 
 /* Reroutes an event to current_math object */
@@ -173,16 +173,16 @@ function install_paste_handler() {
   window.addEventListener("paste",function(event) {
     try {
       console.log("paste event",current_math,event,event.clipboardData.getData('text/plain'));
-        if (current_math!==null) {
-/*	  console.log("discarding paste event");
+      if (current_math!==null) {
+/*          console.log("discarding paste event");
           event.stopPropagation();
           event.preventDefault();
           event.stopImmediatePropagation();
           current_math.focus();
-	  current_math.paste("hello"); */
-	  reroute_event(event);
-	  return;
-	}
+          current_math.paste("hello"); */
+        reroute_event(event);
+        return;
+      }
     } catch (e) {
       console.error(e);
     }
@@ -192,49 +192,49 @@ function install_paste_handler() {
 
 
 function install_key_handler() {
-    window.addEventListener("keydown",function(event) {
-	try {
-        // If there is an active math editor, dispatch keydown events directly to that math editor
-        // This avoids triggering key events of the webpage
-        if (current_math!==null) {
-	  reroute_event(event);
-	  return;
+  window.addEventListener("keydown",function(event) {
+    try {
+      // If there is an active math editor, dispatch keydown events directly to that math editor
+      // This avoids triggering key events of the webpage
+      if (current_math!==null) {
+        reroute_event(event);
+        return;
+      }
+
+      if (event.ctrlKey && event.keyCode==77) {
+        var img = element_before_cursor();
+        //console.log("before cursor: ",img);
+        if (is_mq_img(img)) {
+          edit_math($(img));
+          return;
         }
-        
-	    if (event.ctrlKey && event.keyCode==77) {
-		var img = element_before_cursor();
-		//console.log("before cursor: ",img);
-		if (is_mq_img(img)) {
-		    edit_math($(img));
-		    return;
-		}
 
-		//console.log("Mathquill: insert math");
-		event.stopPropagation();
+        //console.log("Mathquill: insert math");
+        event.stopPropagation();
 
-		var img = create_math();
-		var sel = document.getSelection();
-		sel.deleteFromDocument();
-		sel.getRangeAt(0).insertNode(img);
-		sel.collapseToEnd();
+        var img = create_math();
+        var sel = document.getSelection();
+        sel.deleteFromDocument();
+        sel.getRangeAt(0).insertNode(img);
+        sel.collapseToEnd();
 
-		edit_math($(img));
-		
-		//console.log("Inserted");
-	    }
+        edit_math($(img));
+
+        //console.log("Inserted");
+      }
     } catch (e) {
-		console.error(e);
-	}
-    }, true);
+      console.error(e);
+    }
+  }, true);
 }
 
 function install_css() {
-    try {
-	var link = $("<link>").attr("rel","stylesheet").attr("type","text/css").attr("href","https://kodu.ut.ee/~unruh/mathquill-0.10.1/mathquill.css");
-	link.appendTo("head");
-	//GM_addStyle(GM_getResourceText("mathquillcss").replace(/url\(font\//g,"url(https://raw.githubusercontent.com/dominique-unruh/mathquill-for-gmail/master/font/"));
+  try {
+    var link = $("<link>").attr("rel","stylesheet").attr("type","text/css").attr("href","https://kodu.ut.ee/~unruh/mathquill-0.10.1/mathquill.css");
+        link.appendTo("head");
+        //GM_addStyle(GM_getResourceText("mathquillcss").replace(/url\(font\//g,"url(https://raw.githubusercontent.com/dominique-unruh/mathquill-for-gmail/master/font/"));
     } catch (e) {
-	console.error(e);
+        console.error(e);
     }
 }
 
@@ -265,15 +265,15 @@ function options_page() {
 
     $("#save").on("click",function () { 
       try {
-	$(".option").each(function () {
-	  var name = this.name;
-	  var value = this.value;
-	  GM_setValue(name,value);
-	});
-	$("#save").attr("disabled","disabled");
-	$(".option").removeClass("changed");
+        $(".option").each(function () {
+          var name = this.name;
+          var value = this.value;
+          GM_setValue(name,value);
+        });
+        $("#save").attr("disabled","disabled");
+        $(".option").removeClass("changed");
       } catch (e) {
-	console.error(e);
+        console.error(e);
       }
     });
 
@@ -290,14 +290,18 @@ if (document.location=="https://kodu.ut.ee/~unruh/mathquill-for-gmail-options.ht
   install_image_click_handler();
   install_key_handler();
   install_paste_handler();
-  GM_registerMenuCommand("MathQuill for GMail - Options",
-			 function() { 
-			   try {
-			     GM_openInTab("https://kodu.ut.ee/~unruh/mathquill-for-gmail-options.html",false);
-			   } catch (e) {
-			     console.error(e);
-			   }},
-			 "q");
+  GM_registerMenuCommand("MathQuill for Gmail - Options",
+                         function() { 
+                           try {
+                             GM_openInTab("https://kodu.ut.ee/~unruh/mathquill-for-gmail-options.html",false);
+                           } catch (e) {
+                             console.error(e);
+                           }},
+                         "q");
 }
 
 console.log("MathQuill script loaded on "+document.location);
+
+// Local Variables:
+// indent-tabs-mode: nil
+// End:
